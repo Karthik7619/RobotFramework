ab589e1f5107a2472dc97dc2d64c0dcfd5925fdb
Karthik7619 <karthik.nexgen@gmail.com>
2023-10-28 20:34:33 +0530

    POM Program- Added Add Store Program

--- PageObjectModel_Project/PageElements/Locator.py
+++ PageObjectModel_Project/PageElements/Locator.py
@@ -2,7 +2,26 @@
 txt_username="id:Email"
 txt_password="id:Password"
 button_Login="xpath://button[normalize-space()='Log in']"
-link_Logout="link text:Logout"
+link_logout="//a[normalize-space()='Logout']"
+
+#Add Store Page Elements
+link_dashboard="xpath://p[normalize-space()='Dashboard']"
+link_collapse="xpath://div[@class='card-header with-border']//i[@class='fas fa-minus']"
+link_expand="xpath://div[@id='configuration-steps-card']//i[@class='fas fa-plus']"
+link_Storeinfo="xpath://a[@class='configuration-step-link store-info-step']//h5[1]"
+link_closesym="xpath://body/div[7]/div[2]/header[1]/button[1]/span[1]"
+text_storename="id:Name"
+text_storeurl="id:Url"
+button_save="name:save"
+button_savecontinue="name:save-continue"
+mouseover_Basic="xpath://span[@class='onoffswitch-inner']"
+mouseover_advanced="xpath://span[@class='onoffswitch-inner']"
+button_Delete="id:store-delete"
+link_backtostore="xpath://a[normalize-space()='back to store list']"
+gettext_todaydate="xpath://div[@class='col-md-4 col-xs-12 text-center']"
+gettext_nopversion="xpath://b[normalize-space()='nopCommerce version 4.60.0']"
+link_nopcommwebsite="xpath://a[normalize-space()='nopCommerce']"
+
 
 
 
